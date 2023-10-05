3cb4d245ddad8c06a0a5e089aebac85be5a17ddb
Conor-Keane <g00391846@atu.ie>
2023-10-05 14:23:38 +0100

    Added code to UserService and added EmailService

---
+++ src/main/java/ie/atu/lab2/lab2/EmailService.java
@@ -0,0 +1,11 @@
+package ie.atu.lab2.lab2;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    public void sendEmail(String to, String message) {
+
+    }
+
+}
--- src/main/java/ie/atu/lab2/lab2/UserService.java
+++ src/main/java/ie/atu/lab2/lab2/UserService.java
@@ -1,4 +1,23 @@
 package ie.atu.lab2.lab2;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final EmailService emailService;
+
+    @Autowired
+    public UserService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    public void registerUser(String username, String email) {
+        // Some user registration logic
+
+        /* Send confirmation email
+        EmailService.sendEmail(email, "Welcome to our platform");
+
+         */
+    }
 }
